6abd20983988ebbce9452f14a295898f7511e666
mirette boules <meritboulos@gmail.com>
2021-11-22 18:11:11 +0200

    first commit

---
+++ resources/views/registration/create.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@include('header')
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+               
+        <form method="POST" action="/done">
+            @csrf
+            <fieldset>
+                <legend style="letter-spacing: 1px; font-family: Helvetica; font-size: 20px; color: #C40000; font-weight: bold;">Sign Up</legend>
+                <label for="name">Name:</label> <br/>
+                    <input type="text" name="name" value="" placeholder="First Name / UserName" title="Enter your First name" />&nbsp
+                    <input type="text" name="lname" value="" placeholder="Last Name" title="Enter your Last name"><br>
+                <span id="fnameerr"></span>&nbsp 
+                <span id="lnameerr"></span><br/>
+    
+                <label for="EmailAdress">Email Adress:</label> <br/>
+                    <input type="email" name="email" value="" placeholder="Enter your E-mail" title="Enter Valid E-Mail" /><br><span id="emailerr"></span><br/>
+                
+                <label for="date-of-birth">Date of birth:</label> <br/><br/>
+                    <input type="date" name="dob" value=""/><br><span id="doberr"></span><br/>
+    
+                <label for="age">Age:</label> <br/>
+                    <input type="number" name="age" value="" min="12" required="required"/><br/><br/>
+    
+                <label for="sex">sex:</label><br/>
+                    <input type="text" name="sex" placeholder="Your Gender" title="Enter your Gender" /><br><span id="sexerr"></span><br/>
+
+                <label for="blood-group">Blood Group:</label> <br/>
+                    <input type="text" name="bldgroup" placeholder="Your Blood Group" title="Enter your Blood Group" /><br><span id="bldgerr"></span><br/>
+                
+                <label for="password">Password:</label> <br/>
+                    <input type="password" name="password" placeholder="Password" title="Password should be of length greater than 8" /><br><span id="passerr"></span><br/>
+                
+                <label for="confirm-password">Confirm Password:</label> <br/>
+                    <input type="password" name="re_password" placeholder="Re-enter Password" title="Type the same Password as above" required="required" /><br><span id="confpasserr"></span><br/>
+    
+                <input type="submit" name="submit" value="Register"  />&nbsp
+                <input type="reset" name="Reset" />
+    
+            </fieldset>
+        </form>
+            
+            
+@include('footer')            
+</html>
