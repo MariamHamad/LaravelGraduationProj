4d2c408f6aa33a5f998ba26f068708a5ee52ae0d
MariamHamad <48105572+MariamHamad@users.noreply.github.com>
2021-11-24 19:34:33 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('footer');
+    return view('signup');
 });
 
 Route::get('/register', 'RegistrationController@create');
