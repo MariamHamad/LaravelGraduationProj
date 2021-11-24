1f1efad62457ebdeca1a6647551a3896a79b0c30
MariamHamad <48105572+MariamHamad@users.noreply.github.com>
2021-11-24 20:31:10 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if(env('REDIRECT_HTTPS')) {
-            $this->app['request']->server->set('HTTPS', true);
-        }
+       //
     }
 
     /**
@@ -24,10 +21,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-           iif(env('REDIRECT_HTTPS')) {
-            $url->formatScheme('https');
-        }
+        //
     }
 }
